f8ce8f64f647961d41b726ef7d481b84443a48b9
WeightScale <kreogen@email.ua>
2016-07-05 20:52:35 +0300

    05.07.16 Класс Bluetooth для работы с настройками

--- src/com/kostya/scales_server_net/BluetoothBaseManager.java
+++ src/com/kostya/scales_server_net/BluetoothBaseManager.java
@@ -18,19 +18,18 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by Kostya on 04.07.2016.
+ * @author Kostya Created by Kostya on 04.07.2016.
  */
 public class BluetoothBaseManager {
     Context mContext;
     private BroadcastReceiverBluetooth broadcastReceiverBluetooth;
     private BluetoothAdapter mBluetoothAdapter;
-    BluetoothSocket socket = null;
     private BufferedReader inputBufferedReader;
     private PrintWriter outputPrintWriter;
     private final Handler handler = new Handler();
     private AcceptThread acceptThread;
     private Timer bluetoothTimeout;
-    private static final int TIMEOUT_BLUETOOTH = 300000;
+    private static final int TIMEOUT_BLUETOOTH = 600000; /** Время для таймера выключения bluetooth. */
     private static final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static  final String NAME = "ServerScales";
     private static final String TAG = BluetoothBaseManager.class.getName();
@@ -42,7 +41,7 @@ public class BluetoothBaseManager {
         if(mBluetoothAdapter == null)
             throw new Exception("Bluetooth adapter missing");
         mBluetoothAdapter.enable();
-
+        /** Флаг таймаут включения bluetooth */
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -50,16 +49,19 @@ public class BluetoothBaseManager {
                     flagTimeout = true;
             }
         }, 5000);
-        while (!mBluetoothAdapter.isEnabled() && !flagTimeout) ;//ждем включения bluetooth
+        while (!mBluetoothAdapter.isEnabled() && !flagTimeout) ;//ждем включения bluetooth или таймаут.
         if(flagTimeout)
             throw new Exception("Timeout enabled bluetooth");
-
+        /** Приемник событий bluetooth*/
         broadcastReceiverBluetooth = new BroadcastReceiverBluetooth();
         IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         intentFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
         mContext.registerReceiver(broadcastReceiverBluetooth, intentFilter);
     }
 
+    /**
+     * Запуск потока для процесса Bluetooth server.
+     */
     public void start(){
         acceptThread = new AcceptThread();
         acceptThread.start();
@@ -67,6 +69,9 @@ public class BluetoothBaseManager {
         bluetoothTimeout.schedule(new TimerProcessBluetooth(), TIMEOUT_BLUETOOTH);
     }
 
+    /**
+     * Таймер для для запуска задачи выключения bluetooth после заданого времени.
+     */
     private class TimerProcessBluetooth extends TimerTask {
         @Override
         public void run() {
@@ -74,6 +79,9 @@ public class BluetoothBaseManager {
         }
     }
 
+    /**
+     * Останавливаем процесс bluetooth server и прочии процессы.
+     */
     public void stop(){
         if (acceptThread != null)
             acceptThread.cancel();
@@ -90,37 +98,46 @@ public class BluetoothBaseManager {
 
     }
 
+    /**
+     * Класс процесса Bluetooth Accept.
+     */
     private class AcceptThread extends Thread {
         private final BluetoothServerSocket mmServerSocket;
         private boolean isClosedSocket = false;
 
         public AcceptThread() {
-
             BluetoothServerSocket tmp = null;
             try {
-                // MY_UUID is the app's UUID string, also used by the client code
                 tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, uuid);
             } catch (IOException e) { }
             mmServerSocket = tmp;
         }
 
         public void run() {
-            // Keep listening until exception occurs or a socket is returned
+            BluetoothSocket socket = null;
+            /** Пока процесс не прерван. */
             while (!Thread.currentThread().isInterrupted()) {
                 try {
-                    socket = mmServerSocket.accept();
+                    socket = mmServerSocket.accept(10000);
                     if (socket != null) {
                         /** Процедура обработки приема и отправки данных. */
-                        processInputInputOutputBuffers(/*socket*/);
+                        processInputInputOutputBuffers(socket);
                     }
                 } catch (Exception e) {
                     Log.d(TAG, e.getMessage());
                 }
-                // If a connection was accepted
-
             }
+            /** Закрываем socket  */
             try {socket.close();} catch (Exception e) {}
             cancel();
+        }
+
+
+        /** Устанавливаем флаг для контроля разрыва socket.
+         * @param closedSocket Флаг.
+         */
+        public void setClosedSocket(boolean closedSocket) {
+            isClosedSocket = closedSocket;
         }
 
         /** Закрываем socket, и вызывает завершение thread. */
@@ -131,28 +148,39 @@ public class BluetoothBaseManager {
             } catch (IOException e) { }
         }
 
+        /** Процедура обработки данных.
+         * @param socket Открытый socket
+         * @throws Exception Исключение если ошибка.
+         */
+        private void processInputInputOutputBuffers(BluetoothSocket socket) throws Exception {
 
-    }
-
-    private void processInputInputOutputBuffers(/*BluetoothSocket socket*/) throws Exception {
-
-        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-        inputBufferedReader = new BufferedReader(inputStreamReader);
-        while (socket.isConnected()){
-            if (inputBufferedReader.ready()){
-                String inputLine = inputBufferedReader.readLine();
-                if (inputLine != null){
-                    Log.d(TAG, "Received message : " + inputLine);
-                    outputPrintWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                    outputPrintWriter.println("ServerScales");
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            inputBufferedReader = new BufferedReader(inputStreamReader);
+            /** Пока socket не разорван. */
+            while (!isClosedSocket){
+                /** Если данные готовы для чтения. */
+                if (inputBufferedReader.ready()){
+                    String inputLine = inputBufferedReader.readLine();
+                    if (inputLine != null){
+                        Log.d(TAG, "Received message : " + inputLine);
+                        outputPrintWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                        outputPrintWriter.println("ServerScales");
+                    }
                 }
+                Thread.sleep(10);
             }
+            /** Закрываем при разрыве socket */
+            inputBufferedReader.close();
+            outputPrintWriter.close();
+            socket.close();
         }
-        inputBufferedReader.close();
-        outputPrintWriter.close();
-        socket.close();
+
     }
 
+
+    /**
+     * Приемник событий Bluetooth.
+     */
     public class BroadcastReceiverBluetooth extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -160,14 +188,11 @@ public class BluetoothBaseManager {
             if (action != null) {
                 switch (action){
                     case BluetoothDevice.ACTION_ACL_CONNECTED:
+                        acceptThread.setClosedSocket(false);
                         Log.d(TAG, action);
                         break;
                     case BluetoothDevice.ACTION_ACL_DISCONNECTED:
-                        try {
-                            socket.close();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        acceptThread.setClosedSocket(true);
                         Log.d(TAG, action);
                         break;
                     default:
